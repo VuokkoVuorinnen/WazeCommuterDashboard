b7a6e16afa54b2c53de2d0066abdc01ba070400c
Patrick Van Brussel <patrick.van.brussel@mediagenix.tv>
2026-01-20 11:46:28 +0100

    fix: increase logging verbosity

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+echo "Main Script: Starting scheduler..."
 # Start the scheduler in the background
 python run.py --scheduler &
 
+echo "Main Script: Starting Gunicorn..."
 # Start Gunicorn
 # Passing arguments from CMD in Dockerfile or defaults
-exec gunicorn --bind 0.0.0.0:8000 --workers 2 run:app
+# --log-level debug: More verbose logs
+# --access-logfile -: Log access to stdout
+# --error-logfile -: Log errors to stderr
+# --capture-output: Capture stdout/stderr from workers
+exec gunicorn --bind 0.0.0.0:8000 --workers 2 --log-level debug --access-logfile - --error-logfile - --capture-output run:app
